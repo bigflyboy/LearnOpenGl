887b3d5aadb50158583efd9517bfa9976c7c1e03
xiaoxiaopolang <1014008793@qq.com>
2017-03-28 14:24:36 +0800

    模型加载进行中

---
+++ GLOnMac/03model/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  03model
+//
+//  Created by 王志远 on 2017/3/28.
+//  Copyright © 2017年 王志远. All rights reserved.
+//
+
+#include <iostream>
+// Std. Includes
+#include <string>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GL includes
+#include <learnopengl/shader.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Other Libs
+#include <SOIL.h>
+#include <learnopengl/filesystem.h>
+
+// Properties
+GLuint screenWidth = 800, screenHeight = 600;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
